87c2072ab15aa80058a901598552f8bd3ea2eac4
helloprof <suluxan.seneca@gmail.com>
2025-03-07 11:29:58 -0500

    channels and videos data (linked via id)

---
+++ data/channels.js
@@ -0,0 +1,20 @@
+window.channels = [
+    {
+        channelID: 1,
+        name: 'MrBeast',
+        imageURL: 'https://yt3.googleusercontent.com/nxYrc_1_2f77DoBadyxMTmv7ZpRZapHR5jbuYe7PlPd5cIRJxtNNEYyOC0ZsxaDyJJzXrnJiuDE=s160-c-k-c0x00ffffff-no-rj',
+        url: 'https://www.youtube.com/@MrBeast',
+    },
+    {
+        channelID: 2,
+        name: 'Daily Dose of Internet',
+        imageURL: 'https://yt3.googleusercontent.com/ytc/AIdro_mVwCEqEf7Z9yboWxEOFXZMQWIRWMmgZy8ILLAj8_VnOfo=s160-c-k-c0x00ffffff-no-rj',
+        url: 'https://www.youtube.com/@DailyDoseOfInternet/videos',
+    },
+    {
+        channelID: 3,
+        name: 'Programming with Mosh',
+        imageURL: 'https://yt3.googleusercontent.com/EXprkFw6nOZgxD2Qf3mPCO3StHkyfdp6Jc8WTPQiZJl13lOmRAgH4FuhTYItT36Pw6-6IEAtnA=s160-c-k-c0x00ffffff-no-rj',
+        url: 'https://www.youtube.com/@programmingwithmosh',
+    },
+]
--- data/videos.js
+++ data/videos.js
@@ -1,4 +1,4 @@
-let videos = [
+window.videos = [
   {
     id: 1,
     title: 'I went back to First Grade for a Day',
@@ -6,9 +6,7 @@ let videos = [
     // tags: [],
     videoURL: 'https://www.youtube.com/watch?v=A-vX1AGBGsc&themeRefresh=1',
     thumbnailURL: null,
-    channelName: 'MrBeast',
-    channelImageURL: 'https://yt3.googleusercontent.com/nxYrc_1_2f77DoBadyxMTmv7ZpRZapHR5jbuYe7PlPd5cIRJxtNNEYyOC0ZsxaDyJJzXrnJiuDE=s160-c-k-c0x00ffffff-no-rj',
-    channelURL: 'https://www.youtube.com/@MrBeast',
+    channelID: 1,
     views: 100000000,
     datePublished: 'Jan 31, 2019'
   },
@@ -19,9 +17,7 @@ let videos = [
     // tags: [],
     videoURL: 'https://www.youtube.com/watch?v=Y9cS08Arymw',
     thumbnailURL: null,
-    channelName: 'Daily Dose of Internet',
-    channelImageURL: 'https://yt3.googleusercontent.com/ytc/AIdro_mVwCEqEf7Z9yboWxEOFXZMQWIRWMmgZy8ILLAj8_VnOfo=s160-c-k-c0x00ffffff-no-rj',
-    channelURL: 'https://www.youtube.com/@DailyDoseOfInternet/videos',
+    channelID: 2,
     views: 1600000,
     datePublished: 'Jan 30, 2025'  },
   {
@@ -31,47 +27,52 @@ let videos = [
     // tags: [],
     videoURL: 'https://www.youtube.com/watch?v=kqtD5dpn9C8&pp=ygULbW9zaCBweXRob24%3D',
     thumbnailURL: null,
-    channelName: 'Programming with Mosh',
-    channelImageURL: 'https://yt3.googleusercontent.com/EXprkFw6nOZgxD2Qf3mPCO3StHkyfdp6Jc8WTPQiZJl13lOmRAgH4FuhTYItT36Pw6-6IEAtnA=s160-c-k-c0x00ffffff-no-rj',
-    channelURL: 'https://www.youtube.com/@programmingwithmosh',
+    channelID: 3,
     views: 20000000,
     datePublished: 'January 2021'  
   },
   {
     id: 4,
-    title: '',
+    title: 'Last to Leave the Circle wins 500,000',
     // description: '',
     // tags: [],
-    videoURL: '',
-    thumbnailURL: '',
-    channelName: '',
-    channelImageURL: '',
-    channelURL: '',
-    views: 0,
-    datePublished: ''  },
+    videoURL: 'https://www.youtube.com/watch?v=zajUgQLviwk',
+    thumbnailURL: null,
+    channelID: 1,
+    views: 123121230,
+    datePublished: 'March 2025'  
+  },
   {
     id: 5,
-    title: '',
+    title: 'Last to Leave the Circle wins 500,000',
     // description: '',
     // tags: [],
-    videoURL: '',
-    thumbnailURL: '',
-    channelName: '',
-    channelImageURL: '',
-    channelURL: '',
-    views: 0,
-    datePublished: ''  },
+    videoURL: 'https://www.youtube.com/watch?v=zajUgQLviwk',
+    thumbnailURL: null,
+    channelID: 1,
+    views: 123121230,
+    datePublished: 'March 2025'  
+  },
   {
     id: 6,
-    title: '',
+    title: 'Last to Leave the Circle wins 500,000',
     // description: '',
     // tags: [],
-    videoURL: '',
-    thumbnailURL: '',
-    channelName: '',
-    channelImageURL: '',
-    channelURL: '',
-    views: 0,
-    datePublished: ''  
-  }
+    videoURL: 'https://www.youtube.com/watch?v=zajUgQLviwk',
+    thumbnailURL: null,
+    channelID: 1,
+    views: 123121230,
+    datePublished: 'March 2025'  
+  },
+  {
+    id: 7,
+    title: 'Last to Leave the Circle wins 500,000',
+    // description: '',
+    // tags: [],
+    videoURL: 'https://www.youtube.com/watch?v=zajUgQLviwk',
+    thumbnailURL: null,
+    channelID: 1,
+    views: 123121230,
+    datePublished: 'March 2025'  
+  },
 ]
