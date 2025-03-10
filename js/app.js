46401d5dab48c657b274472232a523086ca263aa
helloprof <suluxan.seneca@gmail.com>
2025-03-10 11:34:48 -0400

    adding cards and removing the previous content on click of the channel buttons

--- js/app.js
+++ js/app.js
@@ -7,18 +7,34 @@ console.log({ channels, videos }, "App Data");
 
 
 function generateVideos(channelID) {
-    let mainContent = document.querySelector(".main")
+    let videoList = document.querySelector(".video-list")
     let videosByChannel = videos.filter((video) => {
         return video.channelID === channelID
     })
     // let table = document.createElement("table")
     videosByChannel.forEach((video) => {
-        let row = document.createElement("tr")
+        // let row = document.createElement("tr")
+        let videoCard = document.createElement("div")
+        let videoTitle = document.createElement("h3")
+        videoTitle.textContent = video.title
+        // let videoDescription = document.createElement("p")
+        // videoDescription.innerText = video.title
+        let videoImage = document.createElement("img")
+        videoImage.src = video.thumbnailURL
+        // videoImage.className = "video-thumbnail"
+
+        videoCard.className = "video-card"
+        videoCard.appendChild(videoImage)
+        videoCard.appendChild(videoTitle)
+        // videoCard.appendChild(videoDescription)
+
+        videoList.appendChild(videoCard)
     })
 }
 
 function generateChannelsButtons() {
     let mainContent = document.querySelector(".main")
+
     channels.forEach((channel) => {
         // console.log(channel)
         // let channelButton = document.createElement("button")
@@ -30,12 +46,20 @@ function generateChannelsButtons() {
         channelIcon.src = channel.imageURL
 
         channelIcon.addEventListener("click", () => {
+            let videoList = document.querySelector(".video-list")
+            if (videoList) {
+                videoList.innerHTML = ""
+            }  
             generateVideos(channel.channelID)
         })
 
 
         mainContent.appendChild(channelIcon)
+
     })
+    let videoList = document.createElement("div")
+    videoList.className = "video-list"
+    mainContent.appendChild(videoList)
 }
 
 document.addEventListener("DOMContentLoaded", () => {
